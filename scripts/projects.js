d4ec708769f751b34c56e2a38cf6aae2db0ebceb
BRANDON PARKER <Brandon@dkparker.com>
2016-01-21 15:55:55 -0800

    Added functionality to display templates from array

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,16 +1,26 @@
-// function Project (opts) {
-//   this.title = opts.title;
-//   this.authorUrl = opts.authorUrl;
-//   this.publishedOn = opts.publishedOn;
-//   this.body = opts.body;
-// }
-//
-// Project.prototype.toHtml = function() {
-//   var template = Handlebars.compile($('#project.template').text());
-//   return template(this);
-// }
-//
-// projectData.forEach(function(ele) {
-//   var newProject = new Project(ele);
-//   newProject.toHtml($)
-// })
+var display= {};
+
+function Project (opts) {
+  this.title = opts.title;
+  this.authorUrl = opts.authorUrl;
+  this.publishedOn = opts.publishedOn;
+  this.body = opts.body;
+}
+
+Project.prototype.toHtml = function() {
+  console.log('toHtml Start')
+  var template = Handlebars.compile($('#project-template').html());
+  console.log('template created');
+  console.log('result: '+template(this));
+  return template(this);
+}
+
+display.initIndexPage = function() {
+  console.log('Function start')
+  projectData.map(function(ele) {
+    console.log('mapping...')
+    var newProject = new Project(ele);
+    $('#rowHeader').append(newProject.toHtml());
+  });
+  console.log('function end');
+}
