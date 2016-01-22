4560ee0e04722d36f753a2b1c6b1dfd4079f4916
BRANDON PARKER <Brandon@dkparker.com>
2016-01-22 00:29:39 -0800

    fixed CSS with styling and template input

--- data/projectData.js
+++ data/projectData.js
@@ -2,26 +2,29 @@ var projectData = [
 
 {
   title: 'Salmon Cookie Stand',
+  author: 'Brandon Parker',
   authorUrl: 'https://brandon-parker.github.io/cookie-stand',
   publishedOn: 'November 8th, 2015',
   body: 'One of the first projects built with HTML, CSS and JavaScript through the CodeFellows 201 program built in the second week of class.',
-  imgLink:'',
+  imgLink:'images/salmon-cookie.png',
 },
 {
   title: 'Vote Tracker',
+  author: 'Brandon Parker',
   authorUrl: 'https://brandon-parker.github.io/vote-tracker-bp',
   publishedOn: 'November 10th, 2015',
   body: 'Project build on the third week of CodeFellows 201 Program.',
-  imgLink: '',
+  imgLink: 'images/vote-tracker.png',
 
 },
 
 {
-  title: '',
-  authorUrl: '',
-  publishedOn: '',
-  body: '',
-  imgLink:'',
+  title: 'Death Madlibs',
+  author: 'Brandon Parker, Carson Parker, Sawako Ishida, Melissa Giaime',
+  authorUrl: 'http://brandon-parker.github.io/mortality/',
+  publishedOn: 'November 20th, 2015',
+  body: 'Final Project for Code Fellows 201. Team Inevitable Mortality by Brandon Parker, Carson Parker, Sawako Ishida, Melissa Giaime. Our team project is a mad-lib to \'predict\' your death. It is not for children. The stories were written complete, and we later determined which words would make the stories the most ridiculous as possible when replaced with random nouns, verbs, or adjectives. We put together a function that would allow the mad-libs to be filled in by the user input that would transfer the input from local storage on the front page to the results page and populate the correct empty array with each word submitted.',
+  imgLink:'images/madlibs.png',
 
 },
 
--- scripts/projects.js
+++ scripts/projects.js
@@ -2,7 +2,9 @@ var display= {};
 
 function Project (opts) {
   this.title = opts.title;
+  this.author = opts.author;
   this.authorUrl = opts.authorUrl;
+  this.imgLink = opts.imgLink
   this.publishedOn = opts.publishedOn;
   this.body = opts.body;
 }
