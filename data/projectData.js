0ccfbe076a3d4f4c7bd4b9dfab5730b5a9c58c87
BRANDON PARKER <Brandon@dkparker.com>
2016-01-21 17:14:04 -0800

    Initialized CSS for mobile use and bugs

--- data/projectData.js
+++ data/projectData.js
@@ -5,12 +5,15 @@ var projectData = [
   authorUrl: 'https://brandon-parker.github.io/cookie-stand',
   publishedOn: 'November 8th, 2015',
   body: 'One of the first projects built with HTML, CSS and JavaScript through the CodeFellows 201 program built in the second week of class.',
+  imgLink:'',
 },
 {
   title: 'Vote Tracker',
   authorUrl: 'https://brandon-parker.github.io/vote-tracker-bp',
   publishedOn: 'November 10th, 2015',
   body: 'Project build on the third week of CodeFellows 201 Program.',
+  imgLink: '',
+
 },
 
 {
@@ -18,6 +21,8 @@ var projectData = [
   authorUrl: '',
   publishedOn: '',
   body: '',
+  imgLink:'',
+
 },
 
 ];
--- scripts/projects.js
+++ scripts/projects.js
@@ -8,19 +8,13 @@ function Project (opts) {
 }
 
 Project.prototype.toHtml = function() {
-  console.log('toHtml Start')
   var template = Handlebars.compile($('#project-template').html());
-  console.log('template created');
-  console.log('result: '+template(this));
   return template(this);
 }
 
 display.initIndexPage = function() {
-  console.log('Function start')
   projectData.map(function(ele) {
-    console.log('mapping...')
     var newProject = new Project(ele);
     $('#rowHeader').append(newProject.toHtml());
   });
-  console.log('function end');
 }
